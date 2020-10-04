3359424ceb39b17f290f658c65f30983deaa2709
tomatenmark <mh88@mailbox.org>
2020-10-04 17:15:31 +0200

    have to be public due to tests

--- src/main/java/de/mherrmann/tomatofilebackup/chunking/ChecksumEngine.java
+++ src/main/java/de/mherrmann/tomatofilebackup/chunking/ChecksumEngine.java
@@ -2,7 +2,7 @@ package de.mherrmann.tomatofilebackup.chunking;
 
 import org.apache.commons.codec.digest.MurmurHash3;
 
-class ChecksumEngine {
+public class ChecksumEngine {
 
     private ChecksumEngine(){}
 
@@ -13,7 +13,7 @@ class ChecksumEngine {
      * @param length - use <code>length</code> bytes beginning at <code>start</code>
      * @return the MurMurHash3 checksum as upper-case hex-string
      */
-    static String getChecksum(byte[] bytes, int start, int length){
+    public static String getChecksum(byte[] bytes, int start, int length){
         long[] result = MurmurHash3.hash128x64(bytes, start, length, 0);
         return String.format("%016X%016X", result[0], result[1]);
     }
