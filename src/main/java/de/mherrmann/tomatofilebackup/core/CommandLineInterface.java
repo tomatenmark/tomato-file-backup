2ead1476f95ef4ba95f69e62629f99694fd37f51
tomatenmark <mh88@mailbox.org>
2020-10-25 00:55:27 +0200

    implemented most important cli features

---
+++ src/main/java/de/mherrmann/tomatofilebackup/core/CommandLineInterface.java
@@ -0,0 +1,115 @@
+package de.mherrmann.tomatofilebackup.core;
+
+import de.mherrmann.tomatofilebackup.Constants;
+import de.mherrmann.tomatofilebackup.core.actions.Action;
+import de.mherrmann.tomatofilebackup.core.actions.ActionEngine;
+import de.mherrmann.tomatofilebackup.exceptions.IllegalCommandException;
+
+public class CommandLineInterface {
+
+    boolean isHelp(String[] args){
+        return args.length > 0 && args[0].equals(Constants.HELP);
+    }
+
+    Command parseArgs(String[] args){
+        if(args.length == 0){
+            throw new IllegalCommandException(Constants.ErrorReport.TOO_FEW_ARGUMENTS.getMessage());
+        }
+        String action = args[0];
+        Command command = new Command();
+        try {
+            command.setActionEngine(getActionEngine(action));
+        } catch(IllegalArgumentException exception){
+            throw new IllegalCommandException(Constants.ErrorReport.INVALID_ACTION.getMessage(action));
+        }
+
+        parseOptions(args, command);
+        return command;
+    }
+
+    public void showGeneralHelp(){
+        StringBuilder help = new StringBuilder();
+        help.append(Constants.TFB_INTRO).append("\n");
+        help.append(" Usage: tfb ACTION [OPTIONS]\n");
+        help.append("  Example: tfb backup --repository=/mnt/backup/repo/ -n /home/max/\n");
+        help.append("\n");
+        help.append(" Execute tfb help ACTION for action specific help\n");
+        help.append("  Example: tfb help backup\n");
+        help.append("\n");
+        help.append(" Actions: ");
+        boolean first = true;
+        for (Action action : Action.values()){
+            if(!first){
+                help.append(", ");
+            }
+            help.append(action.name());
+            first = false;
+        }
+        stdOut(help.toString());
+    }
+
+    void showActionHelp(String[] args){
+        if(args.length < 2){
+            throw new IllegalCommandException(Constants.ErrorReport.MISSING_ACTION.getMessage());
+        }
+        String action = args[1];
+        try {
+            ActionEngine actionEngine = Action.valueOf(action).getAction();
+            showActionHelp(actionEngine);
+        } catch(IllegalArgumentException exception){
+            throw new IllegalCommandException(Constants.ErrorReport.INVALID_ACTION.getMessage(action));
+        }
+    }
+
+    void showActionHelp(Command command){
+        stdOut(command.getActionHelpMessage());
+    }
+
+    void showActionHelp(ActionEngine actionEngine){
+        stdOut(actionEngine.getActionHelpMessage());
+    }
+
+    public void stdOut(String message){
+        System.out.println(message);
+    }
+
+    void stdErr(String message){
+        System.err.println(message);
+    }
+
+    private ActionEngine getActionEngine(String action){
+        return Action.valueOf(action).getAction();
+    }
+
+    private void parseOptions(String[] args, Command command){
+        for(int i = 1; i < args.length; i++){
+            String arg = args[i];
+            if(arg.startsWith("--")){
+                addProperty(arg, command);
+            } else if(arg.startsWith("-")){
+                enableSwitch(arg, command);
+            } else {
+                command.addMainValue(arg);
+            }
+        }
+    }
+
+    private void addProperty(String arg, Command command){
+        String propertyParsePattern = "^--([^=]*)=([^=]*)$";
+        if(!arg.matches(propertyParsePattern)){
+            throw new IllegalCommandException(Constants.ErrorReport.INVALID_ARGUMENT.getMessage(arg));
+        }
+        String key = arg.replaceFirst(propertyParsePattern, "$1");
+        String value = arg.replaceFirst(propertyParsePattern, "$2");
+        command.addProperty(key, value);
+    }
+
+    private void enableSwitch(String arg, Command command){
+        if(arg.length() == 1){
+            throw new IllegalCommandException(Constants.ErrorReport.INVALID_ARGUMENT.getMessage(arg));
+        }
+        for(int i = 1; i < arg.length(); i++){
+            command.enableSwitch(String.valueOf(arg.charAt(i)));
+        }
+    }
+}
---
+++ src/test/java/de/mherrmann/tomatofilebackup/core/CommandLineInterfaceTest.java
@@ -0,0 +1,204 @@
+package de.mherrmann.tomatofilebackup.core;
+
+import de.mherrmann.tomatofilebackup.Constants;
+import de.mherrmann.tomatofilebackup.core.actions.InitializeActionEngine;
+import de.mherrmann.tomatofilebackup.exceptions.IllegalCommandException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandLineInterfaceTest {
+
+    private final PrintStream standardOut = System.out;
+    private final PrintStream standardErr = System.err;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        System.setErr(new PrintStream(errStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+        System.setErr(standardErr);
+    }
+
+    @Test
+    void shouldReturnTrueForIsHelp(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+
+        boolean help = commandLineInterface.isHelp(new String[]{"help"});
+
+        assertTrue(help);
+    }
+
+    @Test
+    void shouldReturnFalseForIsHelpOtherAction(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+
+        boolean help = commandLineInterface.isHelp(new String[]{"other"});
+
+        assertFalse(help);
+    }
+
+    @Test
+    void shouldReturnFalseForIsHelpNoAction(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+
+        boolean help = commandLineInterface.isHelp(new String[]{});
+
+        assertFalse(help);
+    }
+
+    @Test
+    void shouldShowInitializeActionHelpByArgs(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+
+        commandLineInterface.showActionHelp(new String[]{"help", "initialize"});
+
+        assertEquals(new InitializeActionEngine().getActionHelpMessage(), outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void shouldShowInitializeActionHelpByActionEngine(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        InitializeActionEngine engine = new InitializeActionEngine();
+
+        commandLineInterface.showActionHelp(engine);
+
+        assertEquals(engine.getActionHelpMessage(), outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void shouldShowInitializeActionHelpByCommand(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        Command command = new Command();
+        InitializeActionEngine engine = new InitializeActionEngine();
+        command.setActionEngine(engine);
+
+        commandLineInterface.showActionHelp(command);
+
+        assertEquals(engine.getActionHelpMessage(), outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void shouldShowGeneralHelp(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+
+        commandLineInterface.showGeneralHelp();
+
+        assertTrue(outputStreamCaptor.toString().trim().startsWith(Constants.TFB_INTRO+"\n Usage: tfb ACTION [OPTIONS]"));
+    }
+
+    @Test
+    void shouldPrintMessageToStdOut(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        String testMessage = "testMessage";
+
+        commandLineInterface.stdOut("testMessage");
+
+        assertEquals(testMessage, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void shouldPrintMessageToStdErr(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        String testMessage = "testMessage";
+
+        commandLineInterface.stdErr("testMessage");
+
+        assertEquals(testMessage, errStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void shouldParseArgs(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        Map<String, String> expectedProperties = new HashMap<>();
+        expectedProperties.put("test", "testProperty");
+
+
+        Command command = commandLineInterface.parseArgs(new String[]{"initialize", "-a", "-bc", "--test=testProperty", "test1", "test2"});
+
+        assertEquals(InitializeActionEngine.class, command.actionEngine.getClass());
+        assertLinesMatch(Arrays.asList("a", "b", "c"), command.enabledSwitches);
+        assertIterableEquals(expectedProperties.keySet(), command.properties.keySet());
+        assertIterableEquals(expectedProperties.values(), command.properties.values());
+        assertLinesMatch(Arrays.asList("test1", "test2"), command.mainValues);
+    }
+
+    @Test
+    void shouldFailParseArgsCausedByNoAction(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        Exception exception = null;
+
+        try {
+            commandLineInterface.parseArgs(new String[]{});
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        assertNotNull(exception);
+        assertEquals(IllegalCommandException.class, exception.getClass());
+        assertEquals(Constants.ErrorReport.TOO_FEW_ARGUMENTS.getMessage(), exception.getMessage());
+    }
+
+    @Test
+    void shouldFailParseArgsCausedByInvalidAction(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        Exception exception = null;
+
+        try {
+            commandLineInterface.parseArgs(new String[]{"invalid"});
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        assertNotNull(exception);
+        assertEquals(IllegalCommandException.class, exception.getClass());
+        assertEquals(Constants.ErrorReport.INVALID_ACTION.getMessage("invalid"), exception.getMessage());
+    }
+
+    @Test
+    void shouldFailParseArgsCausedByInvalidArgumentSingleDash(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        Exception exception = null;
+
+        try {
+            commandLineInterface.parseArgs(new String[]{"initialize", "-"});
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        assertNotNull(exception);
+        assertEquals(IllegalCommandException.class, exception.getClass());
+        assertEquals(Constants.ErrorReport.INVALID_ARGUMENT.getMessage("-"), exception.getMessage());
+    }
+
+    @Test
+    void shouldFailParseArgsCausedByInvalidArgumentMalformedProperty(){
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+        Exception exception = null;
+
+        try {
+            commandLineInterface.parseArgs(new String[]{"initialize", "--prop"});
+        } catch(Exception ex){
+            exception = ex;
+        }
+
+        assertNotNull(exception);
+        assertEquals(IllegalCommandException.class, exception.getClass());
+        assertEquals(Constants.ErrorReport.INVALID_ARGUMENT.getMessage("--prop"), exception.getMessage());
+    }
+
+}
