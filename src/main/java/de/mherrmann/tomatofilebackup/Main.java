56158c1c17c73e6a2a78542efded826b338ca484
tomatenmark <mh88@mailbox.org>
2020-10-25 00:56:51 +0200

    adding Main method

---
+++ src/main/java/de/mherrmann/tomatofilebackup/Main.java
@@ -0,0 +1,10 @@
+package de.mherrmann.tomatofilebackup;
+
+import de.mherrmann.tomatofilebackup.core.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.run(args);
+    }
+}
